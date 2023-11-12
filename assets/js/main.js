d800c451fe23244fcfc0bac263a7972608ae97f4
quynhtubi3 <idkr37@gmail.com>
2023-11-12 12:46:10 +0700

    hehe

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,11 @@ function closeMenu() {
   const lstMenu = document.querySelector(".lstMenu");
   lstMenu.classList.add("hidden");
 }
+function showSignInScr() {
+  const scr = document.querySelector(".signInScr");
+  scr.classList.remove("hidden");
+}
+function closeSignInScr() {
+  const scr = document.querySelector(".signInScr");
+  scr.classList.add("hidden");
+}
