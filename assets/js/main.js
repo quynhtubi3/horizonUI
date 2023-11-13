b7669875893c69fd4425e558eb7c1224edbcd2fa
quynhtubi3 <idkr37@gmail.com>
2023-11-13 10:21:28 +0700

    hehe

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,37 @@ function closeSignInScr() {
   const scr = document.querySelector(".signInScr");
   scr.classList.add("hidden");
 }
+
+document.addEventListener("scroll", function () {
+  var scrollPosition = window.scrollY || document.documentElement.scrollTop;
+  const lstTitle = document.querySelectorAll(".lstTitle .title");
+
+  if (scrollPosition >= 0 && scrollPosition <= 600) {
+    lstTitle.forEach((data) => {
+      data.classList.remove("active-title");
+    });
+    lstTitle[0].classList.add("active-title");
+  } else if (scrollPosition > 600 && scrollPosition <= 1400) {
+    lstTitle.forEach((data) => {
+      data.classList.remove("active-title");
+    });
+    lstTitle[1].classList.add("active-title");
+  } else if (scrollPosition > 1400 && scrollPosition <= 2100) {
+    lstTitle.forEach((data) => {
+      data.classList.remove("active-title");
+    });
+    lstTitle[2].classList.add("active-title");
+  } else if (scrollPosition > 2100 && scrollPosition <= 2800) {
+    lstTitle.forEach((data) => {
+      data.classList.remove("active-title");
+    });
+    lstTitle[3].classList.add("active-title");
+  } else {
+    lstTitle.forEach((data) => {
+      data.classList.remove("active-title");
+    });
+  }
+  lstTitle.forEach((data) => {
+    console.log(data);
+  });
+});
